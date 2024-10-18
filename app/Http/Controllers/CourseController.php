373d272ae8355042a79d85bee97f915d5fa64dff
Aidana <justaidana99@gmail.com>
2024-10-19 00:56:46 +0500

    Revert "filtering cources"
    This reverts commit 7414e2410c5c6c6d7ba8016487d3f0dc26ae9965.

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -1,94 +1,84 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\Course;
-    use Illuminate\Http\Request;
+use App\Models\Course;
+use Illuminate\Http\Request;
 
-    class CourseController extends Controller
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        /**
-         * Display a listing of the resource.
-         */
-        public function index(Request $request)
-        {
-            // Получаем имя курса из параметров запроса
-            $name = $request->query('name');
-
-            // Если имя курса указано, фильтруем по нему
-            if ($name) {
-                $courses = Course::where('name', 'like', '%' . $name . '%')->get();
-            } else {
-                $courses = Course::all();
+        $courses = Course::all();
         return response()->json($courses);
-        }   }
-
-
-
-
-        /**
-         * Store a newly created resource in storage.
-         */
-        public function store(Request $request)
-        {
-            //
-        }
-
-        /**
-         * Display the specified resource.
-         */
-        public function show(string $id)
-        {
-            $course = Course::with([
-                'category:id,title',
-                'course_modules.lessons:id,course_module_id,title'
-            ])->findOrFail($id);
-
-            return response()->json($course);
-        }
-
-        /**
-         * Update the specified resource in storage.
-         */
-        public function update(Request $request, string $id)
-        {
-            //
-        }
-
-        /**
-         * Remove the specified resource from storage.
-         */
-        public function destroy(string $id)
-        {
-            //
-        }
-
-        public function buy($id)
-        {
-            $course = Course::findOrFail($id);
-            $user = Auth::user();
-
-            if ($user->courses()->where('course_id', $id)->exists()) {
-                return response()->json([
-                    'message' => 'У вас уже есть этот курс.'
-                ], 400);
-            }
-
-            if ($user->balance < $course->price) {
-                return response()->json([
-                    'message' => 'Недостаточно средств.'
-                ], 400);
-            }
-
-            $user->balance -= $course->price;
-            $user->save();
-
-            $user->courses()->attach($course);
-
-            return response()->json([
-                'message' => 'Курс успешно куплен.',
-                'course' => $course,
-                'remaining_balance' => $user->balance
-            ], 200);
-        }
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $course = Course::with([
+            'category:id,title',
+            'course_modules.lessons:id,course_module_id,title'
+        ])->findOrFail($id);
+
+        return response()->json($course);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function buy($id)
+    {
+        $course = Course::findOrFail($id);
+        $user = Auth::user();
+
+        if ($user->courses()->where('course_id', $id)->exists()) {
+            return response()->json([
+                'message' => 'У вас уже есть этот курс.'
+            ], 400);
+        }
+
+        if ($user->balance < $course->price) {
+            return response()->json([
+                'message' => 'Недостаточно средств.'
+            ], 400);
+        }
+
+        $user->balance -= $course->price;
+        $user->save();
+
+        $user->courses()->attach($course);
+
+        return response()->json([
+            'message' => 'Курс успешно куплен.',
+            'course' => $course,
+            'remaining_balance' => $user->balance
+        ], 200);
+    }
+}
--- database/migrations/2024_10_18_180206_add_balance_to_users_table.php
+++ database/migrations/2024_10_18_180206_add_balance_to_users_table.php
@@ -25,5 +25,6 @@ return new class extends Migration
             $table->dropColumn('balance');
         });
     }
-        // поиск курса 
+
+    // пупсик
 };
